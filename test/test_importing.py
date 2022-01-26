c3e68e9496a2c5b3bdc9ab670c0c7e15435d7e9b
linyc74 <yclin.python@gmail.com>
2022-01-26 12:01:28 +0800

    import data integrity

--- test/test_importing.py
+++ test/test_importing.py
@@ -1,5 +1,6 @@
 from .setup import TestCase
 from qiime2_pipeline.importing import ImportFeatureTable, ImportFeatureSequence
+from qiime2_pipeline.exporting import ExportFeatureTable, ExportFeatureSequence
 
 
 class TestImportFeatureTable(TestCase):
@@ -12,9 +13,17 @@ class TestImportFeatureTable(TestCase):
 
     def test_main(self):
         actual = ImportFeatureTable(self.settings).main(
-            feature_table_tsv=f'{self.indir}/labeled-feature-table.tsv')
-        expected = f'{self.workdir}/labeled-feature-table.qza'
+            feature_table_tsv=f'{self.indir}/feature-table.tsv')
+        expected = f'{self.workdir}/feature-table.qza'
         self.assertFileExists(expected, actual)
+
+    def test_data_integrity(self):
+        qza = ImportFeatureTable(self.settings).main(
+            feature_table_tsv=f'{self.indir}/feature-table.tsv')
+        actual = ExportFeatureTable(self.settings).main(
+            feature_table_qza=qza)
+        expected = f'{self.indir}/feature-table-from-qza.tsv'
+        self.assertFileEqual(expected, actual)
 
 
 class TestImportFeatureSequence(TestCase):
@@ -27,6 +36,12 @@ class TestImportFeatureSequence(TestCase):
 
     def test_main(self):
         actual = ImportFeatureSequence(self.settings).main(
-            feature_sequence_fa=f'{self.indir}/labeled-feature-sequence.fa')
-        expected = f'{self.workdir}/labeled-feature-sequence.qza'
+            feature_sequence_fa=f'{self.indir}/feature-sequence.fa')
+        expected = f'{self.workdir}/feature-sequence.qza'
         self.assertFileExists(expected, actual)
+
+    def test_data_integrity(self):
+        input_fa = f'{self.indir}/feature-sequence.fa'
+        qza = ImportFeatureSequence(self.settings).main(feature_sequence_fa=input_fa)
+        output_fa = ExportFeatureSequence(self.settings).main(feature_sequence_qza=qza)
+        self.assertFileEqual(input_fa, output_fa)
