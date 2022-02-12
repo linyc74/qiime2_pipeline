fe3d2375a2f1127372c3ace5948db5c29d3a39c8
linyc74 <yclin.python@gmail.com>
2022-02-12 20:45:22 +0800

    refactor taxon level

--- qiime2_pipeline/taxon_level.py
+++ qiime2_pipeline/taxon_level.py
@@ -7,14 +7,14 @@ from .template import Processor, Settings
 class TaxonLevel(Processor):
 
     OUTPUT_DIRNAME = 'taxon-level'
-    TAXON_KEY_TO_LEVEL = {
-        'phylum': 2,
-        'class': 3,
-        'order': 4,
-        'family': 5,
-        'genus': 6,
-        'species': 7
-    }
+    TAXON_LEVELS = [
+        'phylum',
+        'class',
+        'order',
+        'family',
+        'genus',
+        'species',
+    ]
 
     df: pd.DataFrame
 
@@ -38,17 +38,10 @@ class TaxonLevel(Processor):
 
     def collapse_taxon_at_various_levels(self):
         self.collapsed_dfs = {}
-        for key, level in self.TAXON_KEY_TO_LEVEL.items():
-            self.collapsed_dfs[key] = self.get_collapsed_df(level=level)
-
-    def get_collapsed_df(self, level: int) -> pd.DataFrame:
-        df = self.df.copy(deep=True)
-        df.index = [
-            trim_taxon(taxon=idx, level=level)
-            for idx in self.df.index
-        ]
-        df = df.groupby(level=0).sum()  # collapse by 0-level index
-        return df
+        for level in self.TAXON_LEVELS:
+            self.collapsed_dfs[level] = CollapseTaxon(self.settings).main(
+                df=self.df,
+                level=level)
 
     def save_output_tsvs(self):
         dstdir = f'{self.outdir}/{self.OUTPUT_DIRNAME}'
@@ -61,6 +54,44 @@ class TaxonLevel(Processor):
             self.tsv_dict[key] = tsv
 
 
+class CollapseTaxon(Processor):
+
+    TAXON_LEVEL_STR_TO_INT = {
+        'phylum': 2,
+        'class': 3,
+        'order': 4,
+        'family': 5,
+        'genus': 6,
+        'species': 7
+    }
+
+    df: pd.DataFrame
+    level: str
+
+    def __init__(self, settings: Settings):
+        super().__init__(settings)
+
+    def main(self, df: pd.DataFrame, level: str) -> pd.DataFrame:
+        self.df = df.copy(deep=True)
+        self.level = level
+
+        self.trim_taxon()
+        self.collapse_by_index()
+
+        return self.df
+
+    def trim_taxon(self):
+        int_level = self.TAXON_LEVEL_STR_TO_INT[self.level]
+        self.df.index = [
+            trim_taxon(taxon=taxon, int_level=int_level)
+            for taxon in self.df.index
+        ]
+
+    def collapse_by_index(self):
+        # groupby automatically sorts the index
+        self.df = self.df.groupby(level=0).sum()  # 0-level index
+
+
 def feature_label_to_taxon(s: str) -> str:
     """
     input: 'X; x__AAA; x__BBB; x__CCC; x__DDD; x__EEE; x__FFF; x__GGG'
@@ -70,11 +101,10 @@ def feature_label_to_taxon(s: str) -> str:
     return '|'.join(i[3:] for i in items)
 
 
-def trim_taxon(taxon: str, level: int) -> str:
+def trim_taxon(taxon: str, int_level: int) -> str:
     """
     taxon_hierarchy: '1|2|3|4|5|6|7'
     level: 5
     output: '1|2|3|4|5'
     """
-    return '|'.join(taxon.split('|')[:level])
-
+    return '|'.join(taxon.split('|')[:int_level])
--- test/test_taxon_level.py
+++ test/test_taxon_level.py
@@ -1,6 +1,7 @@
-from .setup import TestCase
+import pandas as pd
 from os.path import exists
-from qiime2_pipeline.taxon_level import TaxonLevel, feature_label_to_taxon, trim_taxon
+from qiime2_pipeline.taxon_level import TaxonLevel, CollapseTaxon, feature_label_to_taxon, trim_taxon
+from .setup import TestCase
 
 
 class TestTaxonLevel(TestCase):
@@ -28,6 +29,23 @@ class TestTaxonLevel(TestCase):
             self.assertTrue(exists(path))
 
 
+class TestCollapseTaxon(TestCase):
+
+    def setUp(self):
+        self.set_up(py_path=__file__)
+
+    def tearDown(self):
+        self.tear_down()
+
+    def test_main(self):
+        actual = CollapseTaxon(self.settings).main(
+            df=pd.read_csv(f'{self.indir}/species.tsv', sep='\t', index_col=0),
+            level='class'
+        )
+        expected = pd.read_csv(f'{self.indir}/class.tsv', sep='\t', index_col=0)
+        self.assertDataFrameEqual(expected, actual)
+
+
 class TestFunction(TestCase):
 
     def test_feature_label_to_taxon(self):
@@ -40,7 +58,7 @@ class TestFunction(TestCase):
     def test_trim_taxon(self):
         actual = trim_taxon(
             taxon='1|2|3|4|5|6|7',
-            level=5
+            int_level=5
         )
         expected = '1|2|3|4|5'
         self.assertEqual(expected, actual)
