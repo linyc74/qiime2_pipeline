ed843360bc9613b0d67929c978a8896ac33a66f6
linyc74 <yclin.python@gmail.com>
2022-05-04 11:23:25 +0800

    proper coloring for big tree

--- qiime2_pipeline/phylogeny.py
+++ qiime2_pipeline/phylogeny.py
@@ -1,37 +1,52 @@
 import os
-from ete3 import Tree, TreeStyle, TextFace
-from typing import Tuple
-from .template import Processor
+from random import randrange
+from typing import Tuple, Dict
+from ete3 import Tree, TreeStyle, NodeStyle, TextFace, RectFace
 from .tools import edit_fpath
+from .template import Processor
 from .exporting import ExportAlignedSequence, ExportTree
 
 
 class Phylogeny(Processor):
 
+    DSTDIR_NAME = 'phylogeny'
+
     seq_qza: str
+
+    dstdir: str
 
     rooted_tree_qza: str
     rooted_tree_nwk: str
     unrooted_tree_nwk: str
 
     def main(self, seq_qza: str) -> str:
+
         self.seq_qza = seq_qza
 
-        self.rooted_tree_qza, self.rooted_tree_nwk, self.unrooted_tree_nwk = \
-            MafftFasttree(self.settings).main(seq_qza=self.seq_qza)
-
-        DrawTree(self.settings).main(nwk=self.rooted_tree_nwk)
-        DrawTree(self.settings).main(nwk=self.unrooted_tree_nwk)
+        self.make_dstdir()
+        self.mafft_fasttree()
+        self.draw_trees()
 
         return self.rooted_tree_qza
+
+    def make_dstdir(self):
+        self.dstdir = f'{self.outdir}/{self.DSTDIR_NAME}'
+        os.makedirs(self.dstdir, exist_ok=True)
+
+    def mafft_fasttree(self):
+        self.rooted_tree_qza, self.rooted_tree_nwk, self.unrooted_tree_nwk = \
+            MafftFasttree(self.settings).main(
+                seq_qza=self.seq_qza,
+                dstdir=self.dstdir)
+
+    def draw_trees(self):
+        for nwk in [self.rooted_tree_nwk, self.unrooted_tree_nwk]:
+            DrawTree(self.settings).main(nwk=nwk, dstdir=self.dstdir)
 
 
 class MafftFasttree(Processor):
 
-    DSTDIR_NAME = 'phylogeny'
-
     seq_qza: str
-
     dstdir: str
 
     aligned_seq_qza: str
@@ -44,20 +59,17 @@ class MafftFasttree(Processor):
     unrooted_tree_nwk: str
     rooted_tree_nwk: str
 
-    def main(self, seq_qza: str) -> Tuple[str, str, str]:
-        self.seq_qza = seq_qza
+    def main(self, seq_qza: str, dstdir: str) -> Tuple[str, str, str]:
 
-        self.make_dstdir()
+        self.seq_qza = seq_qza
+        self.dstdir = dstdir
+
         self.set_output_paths()
         self.execute()
         self.export_to_fa()
         self.export_to_nwk()
 
         return self.rooted_tree_qza, self.rooted_tree_nwk, self.unrooted_tree_nwk
-
-    def make_dstdir(self):
-        self.dstdir = f'{self.outdir}/{self.DSTDIR_NAME}'
-        os.makedirs(self.dstdir, exist_ok=True)
 
     def set_output_paths(self):
         self.aligned_seq_qza = f'{self.workdir}/mafft-aligned-sequences.qza'
@@ -105,39 +117,125 @@ class DrawTree(Processor):
 
     NEWICK_FORMAT = 1  # flexible with internal node names
     QUOTED_NODE_NAMES = True
+    FIG_WIDTH = 200  # mm
+    DPI = 1200
+    LINE_WIDTH = 1
+    LEAF_LENGTH = 100
+    COLORS = [
+        'midnightblue',
+        'royalblue',
+        'cornflowerblue',
+        'purple',
+        'palevioletred',
+        'mediumvioletred',
+        'moccasin',
+        'firebrick',
+        'silver',
+        'turquoise',
+        'yellow',
+        'crimson',
+        'orangered',
+        'darkgreen',
+    ]
 
-    WIDTH = 200  # mm
-    HEIGHT = 200
-    DPI = 600
+    nwk: str
+    dstdir: str
 
-    def main(self, nwk: str):
-        tree = Tree(
-            nwk,
+    tree: Tree
+    phylum_to_color: Dict[str, str]
+
+    def main(self, nwk: str, dstdir: str):
+        self.nwk = nwk
+        self.dstdir = dstdir
+
+        self.set_tree()
+        self.set_phylum_to_color()
+        self.traverse_tree_to_color_leafs()
+        self.render_tree()
+
+    def set_tree(self):
+        self.tree = Tree(
+            self.nwk,
             format=self.NEWICK_FORMAT,
             quoted_node_names=self.QUOTED_NODE_NAMES)
 
-        png = edit_fpath(
-            fpath=nwk,
-            old_suffix='.nwk',
-            new_suffix='.png'
-        )
+    def set_phylum_to_color(self):
+        unique_phylums = set(map(
+            leaf_name_to_bacterial_phylum, self.tree.get_leaf_names()
+        ))
+        self.phylum_to_color = {}
+        for i, phylum in enumerate(unique_phylums):
+            self.phylum_to_color[phylum] = self.COLORS[i] if i < len(self.COLORS) else random_color()
 
-        tree.render(
-            file_name=png,
-            w=self.WIDTH,
-            h=self.HEIGHT,
-            dpi=self.DPI,
-            units='mm',
-            tree_style=MyTreeStyle()
-        )
+    def traverse_tree_to_color_leafs(self):
+        no_node = NodeStyle(
+            size=0,
+            vt_line_width=self.LINE_WIDTH,
+            hz_line_width=self.LINE_WIDTH)
+
+        for node in self.tree.traverse():
+            node.set_style(no_node)
+            if node.is_leaf():
+                phylum = leaf_name_to_bacterial_phylum(node.name)
+                color = self.phylum_to_color[phylum]
+                face = RectFace(
+                    width=self.LEAF_LENGTH,
+                    height=self.LINE_WIDTH,
+                    fgcolor=color,
+                    bgcolor=color)
+                node.add_face(face=face, column=0)
+
+    def render_tree(self):
+        for fmt in ['png', 'pdf', 'svg']:
+            file_name = edit_fpath(
+                fpath=self.nwk,
+                old_suffix='.nwk',
+                new_suffix=f'.{fmt}',
+                dstdir=self.dstdir)
+
+            tree_style = MyTreeStyle(
+                phylum_to_color=self.phylum_to_color)
+
+            self.tree.render(
+                file_name=file_name,
+                w=self.FIG_WIDTH,
+                dpi=self.DPI,
+                units='mm',
+                tree_style=tree_style)
 
 
 class MyTreeStyle(TreeStyle):
 
-    def __init__(self):
+    def __init__(self, phylum_to_color: Dict[str, str]):
         super().__init__()
+
         self.show_leaf_name = False
         self.mode = 'c'
         self.arc_start = -180  # 0 degrees = 3 o'clock
         self.arc_span = 360
-        self.title.add_face(TextFace("Hello ETE", fsize=20), column=0)
+        self.root_opening_factor = 0
+        self.min_leaf_separation = 0
+
+        self.set_phylum_color_legends(phylum_to_color)
+
+    def set_phylum_color_legends(self, phylum_to_color):
+        for phylum, color in phylum_to_color.items():
+            t = TextFace(text=phylum, fgcolor=color, ftype='Arial')
+            self.legend.add_face(t, column=1)
+
+
+def leaf_name_to_bacterial_phylum(leaf_name: str) -> str:
+    """
+    'ASV_0053; d__Bacteria; p__Bacteroidetes; c__Bacteroidia; o__Bacteroidales' -> 'Bacteroidetes'
+    """
+    if '__Bacteria;' not in leaf_name:
+        return 'Unassigned'
+    if '; p__' not in leaf_name:
+        return 'Unassigned'
+    else:
+        return leaf_name.split('; p__')[1].split(';')[0]
+
+
+def random_color() -> str:
+    rgb = tuple(randrange(0, 150) for _ in range(3))
+    return '#%02x%02x%02x' % rgb
--- test/test_phylogeny.py
+++ test/test_phylogeny.py
@@ -1,4 +1,4 @@
-from qiime2_pipeline.phylogeny import Phylogeny, DrawTree
+from qiime2_pipeline.phylogeny import Phylogeny, DrawTree, leaf_name_to_bacterial_phylum
 from .setup import TestCase
 
 
@@ -7,8 +7,8 @@ class TestPhylogeny(TestCase):
     def setUp(self):
         self.set_up(py_path=__file__)
 
-    # def tearDown(self):
-    #     self.tear_down()
+    def tearDown(self):
+        self.tear_down()
 
     def test_main(self):
         actual = Phylogeny(self.settings).main(
@@ -22,10 +22,25 @@ class TestDrawTree(TestCase):
     def setUp(self):
         self.set_up(py_path=__file__)
 
-    # def tearDown(self):
-    #     self.tear_down()
+    def tearDown(self):
+        self.tear_down()
 
     def test_main(self):
-        actual = DrawTree(self.settings).main(
-            nwk=f'{self.indir}/fasttree-rooted.nwk'
-        )
+        DrawTree(self.settings).main(
+            nwk=f'{self.indir}/fasttree-rooted-big.nwk',
+            dstdir=self.outdir)
+
+    def test_leaf_name_to_bacterial_phylum(self):
+        func = leaf_name_to_bacterial_phylum
+
+        leaf_name = 'ASV_0053; d__Bacteria; p__Bacteroidetes; c__Bacteroidia; o__Bacteroidales'
+        self.assertEqual('Bacteroidetes', func(leaf_name))
+
+        leaf_name = 'ASV_0053; d__Bacteria; p__Bacteroidetes'
+        self.assertEqual('Bacteroidetes', func(leaf_name))
+
+        leaf_name = 'ASV_0053; p__Bacteroidetes'
+        self.assertEqual('Unassigned', func(leaf_name))
+
+        leaf_name = 'ASV_0053; d__Bacteria'
+        self.assertEqual('Unassigned', func(leaf_name))
