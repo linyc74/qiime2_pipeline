6fc59053f2b1d669cc765cdc2795495935b6fa17
linyc74 <ylin@nycu.edu.tw>
2024-03-15 19:51:18 +0800

    version 2.3.1

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ import argparse
 import qiime2_pipeline
 
 
-__VERSION__ = '2.3.1-beta'
+__VERSION__ = '2.3.1'
 
 
 PROG = 'python qiime2_pipeline'
--- test/test_qiime2_pipeline.py
+++ test/test_qiime2_pipeline.py
@@ -7,8 +7,8 @@ class TestQiime2Pipeline(TestCase):
     def setUp(self):
         self.set_up(py_path=__file__)
 
-    # def tearDown(self):
-    #     self.tear_down()
+    def tearDown(self):
+        self.tear_down()
 
     def test_paired_end(self):
         Qiime2Pipeline(self.settings).main(
