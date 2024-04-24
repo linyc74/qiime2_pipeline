9a70dc6c501e6d88037a5b4439b7f5c197175b15
linyc74 <ylin@nycu.edu.tw>
2024-04-24 08:08:57 +0800

    clean up

--- test/test_picrust2.py
+++ test/test_picrust2.py
@@ -7,8 +7,8 @@ class TestPICRUSt2(TestCase):
     def setUp(self):
         self.set_up(py_path=__file__)
 
-    # def tearDown(self):
-    #     self.tear_down()
+    def tearDown(self):
+        self.tear_down()
 
     def test_main(self):
         actual = PICRUSt2(self.settings).main(
