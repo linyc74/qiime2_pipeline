f82af6cc6b498efcf1092423b6c9633a9264c97f
linyc74 <ylin@nycu.edu.tw>
2024-04-24 08:07:45 +0800

    bug fix picrust2 when outdir has blank space

--- qiime2_pipeline/picrust2.py
+++ qiime2_pipeline/picrust2.py
@@ -25,6 +25,7 @@ class PICRUSt2(Processor):
         self.remove_existing_dstdir()
         self.run_picrust2()
         self.unzip_pathway_table_tsv()
+        self.move_picrust2_dir()
 
         return self.pathway_table_tsv
 
@@ -54,16 +55,16 @@ class PICRUSt2(Processor):
         df.to_csv(self.feature_table_tsv, sep='\t')
 
     def remove_existing_dstdir(self):
-        if exists(f'{self.outdir}/{self.DSTDIR_NAME}'):
-            self.call(f'rm -r {self.outdir}/{self.DSTDIR_NAME}')  # to avoid directory already exists error
+        if exists(f'{self.workdir}/{self.DSTDIR_NAME}'):
+            self.call(f'rm -r "{self.workdir}/{self.DSTDIR_NAME}"')  # to avoid directory already exists error
 
     def run_picrust2(self):
         log = f'{self.outdir}/picrust2.log'
         args = [
             'picrust2_pipeline.py',
-            f'--study_fasta "{self.feature_sequence_fa}"',
-            f'--input "{self.feature_table_tsv}"',
-            f'--output "{self.outdir}/{self.DSTDIR_NAME}"',
+            f'--study_fasta {self.feature_sequence_fa}',
+            f'--input {self.feature_table_tsv}',
+            f'--output {self.workdir}/{self.DSTDIR_NAME}',
             f'--processes {self.threads}',
             f'1>> "{log}"',
             f'2>> "{log}"',
@@ -74,7 +75,10 @@ class PICRUSt2(Processor):
         self.pathway_table_tsv = f'{self.outdir}/picrust2-pathway-table.tsv'
         args = [
             'gunzip --keep --stdout',
-            f'"{self.outdir}/{self.DSTDIR_NAME}/pathways_out/path_abun_unstrat.tsv.gz"',
+            f'{self.workdir}/{self.DSTDIR_NAME}/pathways_out/path_abun_unstrat.tsv.gz',
             f'> "{self.pathway_table_tsv}"'
         ]
         self.call(self.CMD_LINEBREAK.join(args))
+
+    def move_picrust2_dir(self):
+        self.call(f'mv "{self.workdir}/{self.DSTDIR_NAME}" "{self.outdir}/"')
