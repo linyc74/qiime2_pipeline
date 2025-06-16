511ce38e0f731d4418947554507b4d5804cd4b71
linyc74 <ylin@nycu.edu.tw>
2025-06-16 10:05:11 +0800

    version 2.9.3

--- test/test_taxon_barplot.py
+++ test/test_taxon_barplot.py
@@ -8,8 +8,8 @@ class TestPlotTaxonBarplots(TestCase):
     def setUp(self):
         self.set_up(py_path=__file__)
 
-    # def tearDown(self):
-    #     self.tear_down()
+    def tearDown(self):
+        self.tear_down()
 
     def test_main(self):
         self.settings.for_publication = False
